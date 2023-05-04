6ae786da71d3db4606d7c81a313a3f75079541c3
Prakhar Singh <mrprakharcse01@gmail.com>
2023-05-04 22:59:32 +0530

    Add files via upload

---
+++ Student Dashboard main/timeTable.js
@@ -0,0 +1,137 @@
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '004-D',
+        subject: 'Engg. Maths',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '004-D',
+        subject: 'VAC',
+        type: 'Lecture'
+    },
+    {   time: '11-12 AM',
+        roomNumber: '004-D',
+        subject: 'OS',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '004-D',
+        subject: 'MICRO',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-11 AM',
+        roomNumber: '004D(B)',
+        subject: 'DAA lab',
+        type: 'LAB'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '004-D',
+        subject: 'OS',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '004D',
+        subject: 'TAFL',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {
+       time: '09-10 AM',
+       roomNumber: '004-D',
+       subject: 'Engg. Maths',
+       type: 'Lecture'
+    },
+    {   
+        time: '10-12 AM',
+        roomNumber: '402-D',
+        subject: 'MICRO Lab',
+        type: 'LAB'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '004-D',
+        subject: 'TAFL',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {
+        time: '09-10 AM',
+        roomNumber: '004-D',
+        subject: 'Engg. Maths',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11AM',
+        roomNumber: '004-D',
+        subject: 'DAA',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '004-D',
+        subject: 'OS',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '004-D',
+        subject: 'TAFL',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '004-D',
+        subject: 'MICRO',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '004-D',
+        subject: 'EVS',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '004-D',
+        subject: 'TC',
+        type: 'Lecture'
+    },
+    {
+            time: '12-01 PM',
+            roomNumber: '004-D',
+            subject: 'TAFL',
+            type: 'Lecture'
+        }
+]
+
+const Saturday =[
+    {   
+        time: '10-12 AM',
+        roomNumber: '202-D',
+        subject: 'Mini Project',
+        type: 'Practical'
+    }
+]
+const Sunday =[
+    {
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
